3dc2f210c9085c1ae0b2de65483d4c509dd8fc31
EmirhanServeren <emirhanserveren@gmail.com>
2023-06-12 18:32:37 +0300

    committed changes on Streamlit

---
+++ main_app/pages/Segmentation.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# rename page name
+st.set_page_config(page_title="Customer Segmentation",layout="wide")
+
+st.header("Customer Segmentation")   # page header
--- main_app/pages/cluster.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-# rename page name
-st.set_page_config(page_title="Customer Segmentation",layout="wide")
-
-st.header("Customer Segmentation-ML")   # page header
-st.write("This is a web-app for customer segmentation project.")
