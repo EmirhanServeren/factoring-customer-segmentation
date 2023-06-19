401062bbc8c8a959bde320d15c12ad166fad2848
CuguLee <mert-oguz@hotmail.com>
2023-06-19 18:28:16 +0300

    T type distribution

--- main_app/pages/Segmentation.py
+++ main_app/pages/Segmentation.py
@@ -95,3 +95,55 @@ with tabG:
     # render the chart
     column_2.plotly_chart(fig_bk_check_income, use_container_width=True)
 
+with tabT:
+    # create column containers for the next section
+    colup_1, colup_2 = st.columns(2)
+
+    # create a donut chart for distribution of segments
+    tk_cluster_distribution = pd.read_feather('streamlit_view/tk_cluster_distribution')
+    # header and context at the top
+    colup_1.subheader("Observe the Segment Distribution. Discover Customer Reliability.")
+    # give a info under the header
+    colup_1.markdown("""<p style='color: #FF8585; font-style: bold; font-size: 16px;'>Less number of segments generated then pre-defined labels
+                by the company. Optimized the distribution while reducing number of segments.</p>""", unsafe_allow_html=True)
+    # visualize here
+    fig_tk_distribution = go.Figure(data=[go.Pie(labels=tk_cluster_distribution['Segment'],
+                    values=tk_cluster_distribution['count'])])
+    # move legend to left of chart
+    fig_tk_distribution.update_traces(hole=0.4, hovertemplate=None, hoverinfo='skip',marker=dict(colors=['#F6F314','#2CF5D5','#F50000']))
+    fig_tk_distribution.update_layout(legend=dict(orientation="v",x=0,y=1,xanchor="left",yanchor="bottom"))
+    # render the chart
+    colup_1.plotly_chart(fig_tk_distribution, use_container_width=True)
+
+    # give a context about chart in the head of next column container
+    colup_2.markdown("""**T-Gold is the most reliable segment.** T-Diamond is the moderately reliable segment. **T-Bronze is the most risky segment for T type customers.**""")
+    # print the numerical visualization right to the chart as a data frame
+    tk_cluster_distribution= tk_cluster_distribution.rename(columns={'Count':'Number of Customers'})
+    colup_2.dataframe(tk_cluster_distribution.set_index('Segment'), use_container_width=True)
+    # give the remaining context under the data frame
+    colup_2.markdown("""<p style='color: #FFFF00; font-style: bold; font-size: 18px;'>There are going to be three cases.
+                Low-rated offers ought to be presented to the most reliable customer group.</p>""", unsafe_allow_html=True)
+    colup_2.markdown("""<p style='color: #FF3333; font-style: bold; font-size: 18px;'>High-rated offers ought to be presented to the most
+                risky customer group. It is important to remind their checks' playback would be more risky.</p>""", unsafe_allow_html=True)
+    colup_2.markdown("""<p style='color: #FFFFFF; font-style: bold; font-size: 18px;'>Simply, reliable customers' checks are more
+                preferable for the company.</p>""", unsafe_allow_html=True)
+
+    # create another column containers for the next section
+    column_1,column_2,column_3 = st.columns(3)
+
+    # header declared first because it is over the chart
+    column_2.subheader("Reliable Segment has More Income and Less Loss from Checks")
+    # check income-loss percentage chart which is on the left container
+    tk_check_income = pd.read_feather("streamlit_view/tk_cluster_check_income.feather")
+    # create the visualization below
+    fig_tk_check_income = go.Figure(data=[
+        go.Bar(name='Check Income (%)', x=tk_check_income['Segment'], y=tk_check_income['Check Income (%)'],
+            text=tk_check_income['Check Income (%)'].round(2), textposition='auto', marker=dict(color='yellow')),
+        go.Bar(name='Check Loss (%)', x=tk_check_income['Segment'], y=tk_check_income['Check Loss (%)'],
+            text=tk_check_income['Check Loss (%)'].round(2), textposition='auto', marker=dict(color='red'))])
+    # then declare the stack mode
+    fig_tk_check_income.update_layout(xaxis_title=None,yaxis_title='Percentage (%)', barmode='stack')
+    fig_tk_check_income.update_traces(hovertemplate=None, hoverinfo='skip')
+    # render the chart
+    column_2.plotly_chart(fig_tk_check_income, use_container_width=True)
+
